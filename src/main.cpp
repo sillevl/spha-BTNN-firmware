a9a82a2330546a3801080a17ad31cbc6ecc27c0f
Sille Van Landschoot <info@sillevl.be>
2022-12-29 16:21:41 +0100

    refactor to v0.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,80 @@
 #include "mbed.h"
 #include "lib/ButtonManager.h"
+#include "mbed_trace.h"
 
 #include "SixPack.h"
+#include "Messages/Events.h"
+#include "StatusLed.h"
+#include "ActivityLed.h"
+#include "BusVoltage.h"
+#include "TPH.h"
+#include "FeedbackLed.h"
+#include "TLC59108.h"
 
 using namespace SixPackLib;
 
-#include "TLC59108.h"
-#include "tph.h"
+
+#define TRACE_GROUP "APP"
 
 static const uint16_t FirmwareVersion = 0x0010;
 static const uint16_t DeviceType = 0x0010;
 
-static const PinName SDA_PIN = PB_7;
-static const PinName SCL_PIN = PB_6;
-static const PinName STATUS_LED = PB_0;
-static const PinName CAN_TXD = PA_11;
-static const PinName CAN_RXD = PA_12;
-static const PinName SERIAL_RX_PIN = PA_3;
-static const PinName SERIAL_TX_PIN = PA_2;
-static const PinName VIN_ADC = PA_0;
-
 static const PinName BUTTONS[6] = { PA_5, PA_6, PA_7, PA_8, PA_9, PA_10 };
 
-I2C i2c(SDA_PIN, SCL_PIN);
-CAN can(CAN_TXD, CAN_RXD, 500000);
-
-AnalogIn busVoltage(VIN_ADC);
+I2C i2c(MBED_CONF_APP_SDA_PIN, MBED_CONF_APP_SCL_PIN);
+CAN can(MBED_CONF_SIXPACK_CAN_TXD_PIN, MBED_CONF_SIXPACK_CAN_RXD_PIN, MBED_CONF_SIXPACK_CAN_BITRATE);
 
 static const int I2C_ADDRESS = 0x80;
 TLC59108 leds(&i2c,I2C_ADDRESS);
-TPH tph(i2c);
+BME280 bme280(i2c);
 
 // TODO: ButtonManager needs own queue???
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
 ButtonManager buttons(&queue, BUTTONS);
 
-SixPack sixPack(&can);
+SixPack sixPack(&can, &queue);
 
-int main() {
-    printf("*** Niko 4/6 way button ***\r\n");
+FileHandle *mbed_override_console(int fd)
+{
+    static BufferedSerial console(MBED_CONF_APP_UART_TX, MBED_CONF_APP_UART_RX, MBED_CONF_PLATFORM_STDIO_BAUD_RATE);
+    return &console;
+}
 
-    busVoltage.set_reference_voltage(3.3f);
+void initLeds() {
     leds.enable();
     for(int i = 0; i < 6; i++) {
         leds.setChannel(i, 0);
     }
-    
+}
 
-    sixPack.setStatusLed(STATUS_LED);
+int main() {
+    mbed_trace_init();
+    mbed_trace_config_set(TRACE_MODE_COLOR | TRACE_ACTIVE_LEVEL_ALL);
+
+    tr_info("*** Niko 4/6 way button ***");
+
+    initLeds();
+    
     sixPack.setDeviceType(DeviceType);
     sixPack.setFirmwareVersion(FirmwareVersion);
 
-    sixPack.setTPHCallback([]() -> SixPack::TPH {
-        TPHValues values =  tph.getValues();
-        return { (float) values.temperature, (float) values.humidity, (float) values.pressure };
+    ActivityLed commsLed(MBED_CONF_APP_STATUS_LED_PIN);
+    BusVoltage busVoltage(MBED_CONF_APP_VBUS_ADC);
+    TPH tph(&bme280);
+
+    FeedbackLed feedbackLeds;
+    feedbackLeds.onMessage([leds](uint8_t index, uint8_t brightness){
+        leds.setChannel(index, brightness / 255.0f);
     });
 
-    sixPack.setBusVoltageCallback([]() -> float {
-        return busVoltage.read_voltage() * ((200.0f + 10.0f) / 10.0f) * 1.02f;  // calibration factor (tolerance)
-    });
-    
+    sixPack.registerComponent(&commsLed);
+    sixPack.registerComponent(&busVoltage);
+    sixPack.registerComponent(&tph);
+    sixPack.registerComponent(&feedbackLeds);
+
+
     buttons.onStateChange([] (uint8_t index, ButtonManager::ButtonState state, uint32_t time) {
-        sixPack.buttonUpdate( { index, (SixPack::ButtonState) state, time } );
-    });
-
-    // TODO: feedback leds
-    sixPack.feedbackLedsEvent([](SixPack::FeedbackLedStatus status){
-        uint8_t gamma_lookup[] = { 0, 1, 2, 5, 10, 16, 25, 36, 50, 68, 89, 114, 142, 175, 213, 255 };
-        float brightness = gamma_lookup[status.brightness >> 4] / 255.0;
-        leds.setChannel(status.index, brightness);
+        sixPack.send(Events::Button(index, state, time));
     });
 
     queue.dispatch_forever();
