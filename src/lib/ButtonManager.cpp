e567846656274504c4bf2bac1c17bb1dbd355852
Sille Van Landschoot <info@sillevl.be>
2023-02-18 16:23:32 +0100

    make ButtonManager templated

--- src/lib/ButtonManager.cpp
+++ src/lib/ButtonManager.cpp
@@ -1,32 +1,40 @@
 #include "ButtonManager.h"
 
-ButtonManager::ButtonManager(EventQueue* queue, const PinName* pinnames) {
+template <uint8_t N>
+ButtonManager<N>::ButtonManager(EventQueue* queue, const std::array<PinName, N> pinnames) {
     this->queue = queue;
-    for(uint8_t i = 0; i<6; i++) {
+    for(uint8_t i = 0; i<N; i++) {
         InterruptIn* button = new InterruptIn(pinnames[i], PullUp);
-        button->rise(this->queue->event(Callback<void(uint8_t)>(this, &ButtonManager::released), i));
-        button->fall(this->queue->event(Callback<void(uint8_t)>(this, &ButtonManager::pressed), i));
+        button->rise(this->queue->event(Callback<void(uint8_t)>(this, &ButtonManager<N>::released), i));
+        button->fall(this->queue->event(Callback<void(uint8_t)>(this, &ButtonManager<N>::pressed), i));
         buttons[i] = button;
     };
     timer.start();
 }
 
-void ButtonManager::onStateChange(mbed::Callback<void(uint8_t buttonIndex, ButtonState state, uint32_t time)> callback){
+template <uint8_t N>
+void ButtonManager<N>::onStateChange(mbed::Callback<void(uint8_t buttonIndex, ButtonState state, uint32_t time)> callback){
     stateChanged = callback;
 }
 
-void ButtonManager::pressed(uint8_t button) {
+template <uint8_t N>
+void ButtonManager<N>::pressed(uint8_t button) {
     uint32_t now = currentTimeStamp();
     stateChanged(button, PRESSED, now - lastReleaseEventTimeStamp[button]);
     lastPressEventTimeStamp[button] = now;
 }
 
-void ButtonManager::released(uint8_t button) {
+template <uint8_t N>
+void ButtonManager<N>::released(uint8_t button) {
     uint32_t now = currentTimeStamp();
     stateChanged(button, RELEASED, now - lastPressEventTimeStamp[button]);
     lastReleaseEventTimeStamp[button] = now;
 }
 
-uint32_t ButtonManager::currentTimeStamp() {
+template <uint8_t N>
+uint32_t ButtonManager<N>::currentTimeStamp() {
     return duration_cast<milliseconds>(timer.elapsed_time()).count();
 }
+
+template class ButtonManager<4>;
+template class ButtonManager<6>;
--- src/lib/ButtonManager.h
+++ src/lib/ButtonManager.h
@@ -4,23 +4,24 @@
 
 using namespace std::chrono;
 
+template <uint8_t N>
 class ButtonManager {
     public:
         enum ButtonState {  RELEASED = 0, PRESSED = 1 };
 
-        ButtonManager(EventQueue* queue, const PinName* pinnames);
+        ButtonManager(EventQueue* queue, const std::array<PinName, N> pinnames);
         void onStateChange(mbed::Callback<void(uint8_t buttonIndex, ButtonState state, uint32_t time)> cb);
 
     private:
         uint32_t currentTimeStamp();
 
-        InterruptIn* buttons[6];
+        InterruptIn* buttons[N];
         EventQueue* queue;
         mbed::Callback<void(uint8_t buttonIndex, ButtonState state, uint32_t time)> stateChanged;
 
         Timer timer;
-        uint32_t lastPressEventTimeStamp[6]= { 0 };
-        uint32_t lastReleaseEventTimeStamp[6]= { 0 };
+        uint32_t lastPressEventTimeStamp[N]= { 0 };
+        uint32_t lastReleaseEventTimeStamp[N]= { 0 };
 
         void released(uint8_t index);
         void pressed(uint8_t index);
