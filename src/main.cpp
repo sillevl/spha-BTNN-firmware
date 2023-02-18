0a91362f5f3ff1f5f8e3de4b15f0794fd2e5d3a4
Sille Van Landschoot <info@sillevl.be>
2023-02-18 22:16:12 +0100

    refactor code

--- src/main.cpp
+++ src/main.cpp
@@ -16,29 +16,26 @@ using namespace SixPackLib;
 
 #define TRACE_GROUP "APP"
 
-static const uint16_t FirmwareVersion = 0x0010;
-static const uint16_t DeviceType = spha::SPHA_BTNU;
-
-static const PinName BUTTONS[6] = { MBED_CONF_APP_BUTTON_1, MBED_CONF_APP_BUTTON_2, MBED_CONF_APP_BUTTON_3, MBED_CONF_APP_BUTTON_4 };
-
-I2C i2c(MBED_CONF_APP_SDA_PIN, MBED_CONF_APP_SCL_PIN);
-CAN can(MBED_CONF_SIXPACK_CAN_TXD_PIN, MBED_CONF_SIXPACK_CAN_RXD_PIN, MBED_CONF_SIXPACK_CAN_BITRATE);
-
-static const int I2C_ADDRESS = 0x80;
-TLC59108 leds(&i2c,I2C_ADDRESS);
-
-// TODO: ButtonManager needs own queue???
+// SixPack
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
-ButtonManager buttons(&queue, BUTTONS, 4);
+SixPack sixPack(&queue, FIRMWARE_VERSION_NUMBER);
 
-SixPack sixPack(&can, &queue);
+// Board drivers
+I2C i2c(MBED_CONF_APP_SDA_PIN, MBED_CONF_APP_SCL_PIN);
+TLC59108 leds(&i2c,MBED_CONF_APP_TLC59116_I2C_ADDRESS);
+ButtonManager buttons(
+    &queue,
+    (const PinName[]) { MBED_CONF_APP_BUTTON_1, MBED_CONF_APP_BUTTON_2, MBED_CONF_APP_BUTTON_3, MBED_CONF_APP_BUTTON_4 },
+    MBED_CONF_APP_NUMBER_OF_BUTTONS
+);
 
-FileHandle *mbed_override_console(int fd)
-{
+// Override stdio
+FileHandle *mbed_override_console(int fd) {
     static BufferedSerial console(MBED_CONF_APP_UART_TX, MBED_CONF_APP_UART_RX, MBED_CONF_PLATFORM_STDIO_BAUD_RATE);
     return &console;
 }
 
+// Initialize LEDs
 void initLeds() {
     leds.enable();
     leds.setBrightness(1.0f);
@@ -48,35 +45,37 @@ void initLeds() {
 }
 
 int main() {
+    // Enable mbed trace
     mbed_trace_init();
     mbed_trace_config_set(TRACE_MODE_COLOR | TRACE_ACTIVE_LEVEL_ALL);
 
-    tr_info("*** SPHA Button Universal ***");
-    tr_info("Firmware version: %s", FIRMWARE_FIRMWARE_VERSION);
+    // show hardware and firmware info
+    tr_info("\n\n*** SPHA Button Universal ***");
+    tr_info("Firmware version: %s", FIRMWARE_VERSION);
     tr_info("Sixpack version: %s", SIXPACK_FIRMWARE_VERSION);
 
+    // Initialize LEDs
     initLeds();
 
-    sixPack.setDeviceType(DeviceType);
-    sixPack.setFirmwareVersion(FirmwareVersion);
-
+    // Initialize Sixpack modules
     ActivityLed commsLed(MBED_CONF_APP_STATUS_LED_PIN);
     BusVoltage busVoltage(MBED_CONF_APP_VBUS_ADC);
-
     FeedbackLed feedbackLeds;
     feedbackLeds.onMessage([leds](uint8_t index, uint8_t brightness){
         leds.setChannel(index, brightness / 255.0f);
     });
 
-    sixPack.registerComponent(&commsLed);
-    sixPack.registerComponent(&busVoltage);
-    sixPack.registerComponent(&feedbackLeds);
-
-
+    // Handle button events
     buttons.onStateChange([] (uint8_t index, ButtonManager::ButtonState state, uint32_t time) {
         tr_info("Button %d: %d, (%d)", index, state, time);
         sixPack.send(Events::Button(index, state, time));
     });
 
+    // Register modules
+    sixPack.registerComponent(&commsLed);
+    sixPack.registerComponent(&busVoltage);
+    sixPack.registerComponent(&feedbackLeds);
+
+    // Handle evens
     queue.dispatch_forever();
 }
